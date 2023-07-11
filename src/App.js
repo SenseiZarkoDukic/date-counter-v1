ce3002a277b5712e30af6632453b960e4ccc125b
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-07-11 18:20:42 +0200

    Created current date variable, dateSec variable that changes current date in seconds, and two state functions for setting steps and counts

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export default function App() {
   return (
     <div className="App">
@@ -7,5 +9,30 @@ export default function App() {
 }
 
 function Counter() {
-  return <div>TEST</div>;
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const stepPlus = () => setStep((s) => s + 1);
+  const stepMinus = () => {
+    if (step > 1) setStep((s) => s - 1);
+  };
+  let date = Date().slice(0, 15);
+  let dataSec = Math.round(new Date().getTime() / 1000);
+
+  return (
+    <>
+      <div>
+        <button onClick={stepMinus}>-</button>Step: {step}
+        <button onClick={stepPlus}>+</button>
+      </div>
+      <div>
+        <button>-</button>Count: {count}
+        <button>+</button>
+      </div>
+      <br />
+      <p>
+        Today is {date} = {dataSec}
+      </p>
+    </>
+  );
 }
